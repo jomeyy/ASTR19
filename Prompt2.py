05911332c6582a91359118fe1c740342ca979f48
jomeyy <156133925+jomeyy@users.noreply.github.com>
2024-01-29 14:25:16 -0800

    Add files via upload

---
+++ Prompt2.py
@@ -0,0 +1,12 @@
+x = 4.0 + 7.3   #sum of floating numbers
+print (x)
+print(type(x))
+
+x = 9 - 2      #subtration of integer
+print(x)
+print (type(x))
+
+x = 3.3 * 7      #the product of a floating point number and an integer #calculate the product #print the product
+print(x)
+print (type(x))
+
