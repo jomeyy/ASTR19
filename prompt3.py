3cd3c5044bb0636986b2def5ef469aef28a7c91c
jomeyy <156133925+jomeyy@users.noreply.github.com>
2024-01-29 19:41:12 -0800

    Add files via upload

---
+++ prompt3.py
@@ -0,0 +1,13 @@
+
+
+def f(x):
+	return **3 + 8 
+def main():
+	x_value = 9
+	result = f(x_value)
+	print(f"The result of f({x_ value}) is: {result}")
+
+	if result > 27:
+		print ("YAY!")
+if __name__== "__main__":
+	main()
